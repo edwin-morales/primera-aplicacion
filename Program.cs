cf641f5f632142c274613d64304e635a93670dab
Edwin Morales <edwinmoralesv@outlook.com>
2022-05-18 11:11:39 -0500

    nuevo 4

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          ConversorEuroDolar obj = new ConversorEuroDolar();
+
+            obj.euro = 1.45;
+
+            Console.WriteLine(obj.convierte(50));
+            
+        }
+    } 
+    
+    class circulo
+    {
+
+       private const double pi = 3.1416;
+
+        public double calculoArea (int radio)
+        {
+
+            return pi * radio * radio;
+
+        }
+
+
+    }
+
+    class ConversorEuroDolar
+    {
+
+       private double euro = 1.253;
+
+        public double convierte(double cantidad)
+        {
+
+            return cantidad * euro;
+
+        }
+        public void cambiarValorEuro(double nuevoValor)
+        {
+            if (nuevoValor < 0) euro = 1.253;
+
+            else
+                euro = nuevoValor;
+
+
+        }
+
+    }
+}
